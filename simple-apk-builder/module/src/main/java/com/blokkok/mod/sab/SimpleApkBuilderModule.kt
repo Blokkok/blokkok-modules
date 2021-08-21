dfc6647e69e56f7442b8a6f7d3ca13185eb760b9
Iyxan23 <nurihsanalghifari@gmail.com>
2021-08-21 14:18:31 +0700

    fix: initialize managers on onLoaded

--- simple-apk-builder/module/src/main/java/com/blokkok/mod/sab/SimpleApkBuilderModule.kt
+++ simple-apk-builder/module/src/main/java/com/blokkok/mod/sab/SimpleApkBuilderModule.kt
@@ -1,11 +1,14 @@
 package com.blokkok.mod.sab
 
+import android.content.Context
 import com.blokkok.mod.aapem.AAPEMImplementation
+import com.blokkok.mod.sab.managers.CommonFilesManager
+import com.blokkok.mod.sab.managers.NativeBinariesManager
 import com.blokkok.modsys.communication.CommunicationContext
 import com.blokkok.modsys.modinter.Module
 
 class SimpleApkBuilderModule : Module() {
-    override val namespace: String get() = "blokkok-simple-code-editor"
+    override val namespace: String get() = "blokkok-simple-apk-builder"
 
     override fun onLoaded(comContext: CommunicationContext) {
         AssetsPaths.init(
@@ -14,6 +17,11 @@ class SimpleApkBuilderModule : Module() {
                     "Simple APK Builder Module's assets doesn't seem to have been extracted"
                 )
         )
+        val context = comContext.invokeFunction("application_context") as Context
+
+        CommonFilesManager.initialize(context, assets!!)
+        NativeBinariesManager.initialize(context, assets!!)
+
         AAPEMImplementation.registerImplementation(SABApkBuilderImpl())
     }
 
